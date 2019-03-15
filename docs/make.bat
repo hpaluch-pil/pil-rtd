17e56cac64226d2594563539b9275cda4f7e8ae1
Henryk Paluch <henryk.paluch@pickering.cz>
2019-03-15 10:47:20 +0100

    Fixed case when Sphinx path contains spaces (very common)

--- docs/make.bat
+++ docs/make.bat
@@ -12,7 +12,7 @@ set BUILDDIR=build
 
 if "%1" == "" goto help
 
-%SPHINXBUILD% >NUL 2>NUL
+"%SPHINXBUILD%" >NUL 2>NUL
 if errorlevel 9009 (
 	echo.
 	echo.The 'sphinx-build' command was not found. Make sure you have Sphinx
@@ -25,11 +25,11 @@ if errorlevel 9009 (
 	exit /b 1
 )
 
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
+"%SPHINXBUILD%" -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
 goto end
 
 :help
-%SPHINXBUILD% -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
+"%SPHINXBUILD%" -M help %SOURCEDIR% %BUILDDIR% %SPHINXOPTS%
 
 :end
 popd
